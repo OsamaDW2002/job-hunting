6df1666bd29cff0c616213b223dc535fc3245244
OsamaDW <s12028609@stu.najah.edu>
2025-04-11 18:36:08 +0300

    add data base to the project

--- src/js/create-card.js
+++ src/js/create-card.js
@@ -85,39 +85,7 @@ const searchInput = document.getElementById("job-search");
 const noResults = document.getElementById("no-results");
 
 // Example jobs
-const jobs = [
-  {
-    imgUrl: "https://bs-uploads.toptal.io/blackfish-uploads/components/open_graph_image/10233694/og_image/optimized/op-Ten-Front-End-Design-Rules-For-Developers_Luke-Social-33a3a7c9b759fdaa22973906070f8065.png",
-    title: "Frontend Developer",
-    postDate: "1 Apr 2025",
-    candidates: 5,
-    experience: "1 year",
-    salary: "Negotiable",
-    workType: "Full-Time",
-    positionRole: "Developer"
-  },
-  {
-    imgUrl: "https://newline.tech/wp-content/uploads/2023/07/Profession_-Backend-Developer.png",
-    title: "Backend Engineer",
-    postDate: "2 Apr 2025",
-    candidates: 10,
-    experience: "3 years",
-    salary: "$4000/month",
-    workType: "Remote",
-    positionRole: "Engineer"
-  },
-  {
-    imgUrl: "https://www.keenesystems.com/hs-fs/hubfs/blog-images/ux-design.jpg?width=900&name=ux-design.jpg",
-    title: "UX Designer",
-    postDate: "3 Apr 2025",
-    candidates: 3,
-    experience: "2 years",
-    salary: "Negotiable",
-    workType: "Part-Time",
-    positionRole: "Designer"
-  }
-];
-
+let jobs = JSON.parse(localStorage.getItem('data'))
 // Render jobs
 jobs.forEach(job => createJobCard(job, jobList));
 
