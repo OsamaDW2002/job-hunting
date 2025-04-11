3b27af1b84cdf0de3b945c7baaa4ea95de64ca04
OsamaDW <s12028609@stu.najah.edu>
2025-04-11 21:51:30 +0300

    edit section

---
+++ src/js/edit-section-loading.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    addSkill('UI');
+    addSkill('UX');
+    addSkill('Html');
+    addSchedule('Monday to Friday');
+    addSchedule('Weekend availability');
+    addLocation('United Kingdom');
+    addLocation('Canada')
+})
